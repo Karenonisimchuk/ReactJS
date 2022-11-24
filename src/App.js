5596e31763dbc56571c881817b59cb6292501aca
Karen Onisimchuk <83030574+Karenonisimchuk@users.noreply.github.com>
2022-11-23 23:26:10 -0300

    Segunda pre-entrega

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Layout } from './components/Layout';
+import ItemListContainer from './components/ItemListContainer';
+
+function App() {
+  return (
+    <Layout>
+      <ItemListContainer />
+    </Layout>
+  );
+}
+
+export default App;
